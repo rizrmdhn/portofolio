49ae8130c14d7a8376b3a94af12e21f304ada3f7
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-05-12 14:20:16 +0800

    fix(utils): ensure correct base URL for Vercel deployment
    - added check for VERCEL_URL to return the correct URL

--- apps/web/src/utils/get-base-url.ts
+++ apps/web/src/utils/get-base-url.ts
@@ -1,4 +1,5 @@
 export const getBaseUrl = (): string => {
   if (typeof window !== "undefined") return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3001";
 };
