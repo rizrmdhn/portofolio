2305c4c2e985421c37fda7fce0e1d200689b0f84
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-05-12 12:52:15 +0800

    feat(public): add new icons and web manifest for improved branding
    - added android-chrome-192x192.png
    - added android-chrome-512x512.png
    - added apple-touch-icon.png
    - added favicon-16x16.png
    - added favicon-32x32.png
    - added favicon.ico
    - added site.webmanifest for PWA support
    - updated __root.tsx to include new icons and manifest

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -30,7 +30,25 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
     ],
     links: [
       { rel: 'stylesheet', href: appCss },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+      { rel: 'icon', href: '/favicon.ico' },
     ],
   }),
 
